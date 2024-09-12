2b34137976a2466f1eaf98b3a8921a8e25ff083a
Juraj Paluba <juraj.palubajp@gmail.com>
2024-09-12 21:46:31 +0200

    recipes-rs: implement custom error handling
    It is not perferct but better than matching every Result. It keeps door
    opened for good logging.
    
    TODO: find a way to not duplicate #display macro

--- recipes-rs/src/services/recipes.rs
+++ recipes-rs/src/services/recipes.rs
@@ -1,9 +1,13 @@
-use actix_web::http::header::ContentType;
 use diesel::{query_dsl::methods::SelectDsl, SelectableHelper};
 use diesel_async::{AsyncPgConnection, RunQueryDsl};
 use diesel_async::pooled_connection::deadpool::Pool;
-use actix_web::{get, web, HttpResponse, Responder, error};
 use serde::Serialize;
+use derive_more::derive::{Display, Error};
+use actix_web::{
+    get, web, error,
+    http::{StatusCode, header::ContentType},
+    HttpResponse, Responder
+};
 
 use crate::{models::recipe::Recipe, schema::recipes};
 
@@ -12,29 +16,52 @@ struct ResponseBodyVec<T> {
     pub result: T,
 }
 
+#[derive(Debug, Display, Error)]
+enum ApiErrors {
+    #[display("An internal error occurred. Please try again later.")]
+    InternalError,
+    #[display("An internal error occurred. Please try again later.")]
+    DatabaseConnectionError,
+}
+
+impl error::ResponseError for ApiErrors {
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        match *self {
+            // TODO maybe logging here?
+            ApiErrors::InternalError => println!("Internal Error"),
+            ApiErrors::DatabaseConnectionError => println!("DatabaseConnectionError"),
+        }
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match *self {
+            ApiErrors::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+            ApiErrors::DatabaseConnectionError => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+
 #[get("")]
 pub async fn recipes_list(
     pool: web::Data<Pool<AsyncPgConnection>>,
 ) -> actix_web::Result<impl Responder> {
-    let mut connection = match pool.get().await {
-        Ok(conn) => conn,
-        Err(_) => return Err(error::ErrorInternalServerError("Unable to get connection from pool")),
-    };
+    // TODO: logging
+    let mut connection = pool.get().await.map_err(|_e| ApiErrors::DatabaseConnectionError)?;
 
     let db_result = recipes::table
         .select(Recipe::as_select())
         .load(&mut connection).await;
-    let recipes_vec = match db_result {
-        Ok(recipes) => recipes,
-        Err(_) => return Err(error::ErrorInternalServerError("Cannot get recipes from the database")),
-    };
+    let recipes_vec = db_result.map_err(|_e| ApiErrors::InternalError)?;
+
     let response_body = ResponseBodyVec {
         result: recipes_vec,
     };
-    let response_serialized = match serde_json::to_string(&response_body) {
-        Ok(res) => res,
-        Err(_) => return Err(error::ErrorInternalServerError("Cannot serialize recipes"))
-    };
+    let response_serialized = serde_json::to_string(&response_body)
+        .map_err(|_e| ApiErrors::InternalError)?;
 
     return Ok(
         HttpResponse::Ok()
