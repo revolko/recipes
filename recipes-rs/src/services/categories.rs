fbd8b9286d4cc11ba243f98e2dc0a1618ea05aaa
Juraj Paluba <juraj.palubajp@gmail.com>
2024-10-28 22:56:05 +0100

    feat(recipes-rs): create get category endpoint

--- recipes-rs/src/services/categories.rs
+++ recipes-rs/src/services/categories.rs
@@ -38,6 +38,33 @@ pub async fn categories_list(
         .body(response_serialized));
 }
 
+#[get("/{id}")]
+pub async fn categories_get(
+    pool: web::Data<Pool<AsyncPgConnection>>,
+    path: web::Path<i32>,
+) -> actix_web::Result<impl Responder> {
+    use crate::schema::categories::dsl::*;
+    let category_id = path.into_inner();
+
+    let mut connection = pool
+        .get()
+        .await
+        .map_err(|_e| errors::ApiErrors::DatabaseConnectionError)?;
+
+    let category: Category = categories
+        .find(category_id)
+        .first(&mut connection)
+        .await
+        .map_err(|_e| errors::ApiErrors::NotFound)?;
+    let response_serialized =
+        serde_json::to_string(&category).map_err(|_e| errors::ApiErrors::InternalError)?;
+
+    return Ok(HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(response_serialized));
+}
+
 pub fn categories_config(cfg: &mut web::ServiceConfig) {
     cfg.service(categories_list);
+    cfg.service(categories_get);
 }
