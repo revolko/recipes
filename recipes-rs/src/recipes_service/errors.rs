673ae7747bba04003f9d853addc7040a49b171e1
Juraj Paluba <juraj.palubajp@gmail.com>
2026-06-20 20:32:37 +0200

    refactor(service): use thiserror create for ServiceError

--- recipes-rs/src/recipes_service/errors.rs
+++ recipes-rs/src/recipes_service/errors.rs
@@ -1,21 +1,11 @@
-use derive_more::{Display, Error};
 use diesel::result::Error as DieselError;
 use diesel_async::pooled_connection::deadpool::PoolError;
+use thiserror::Error;
 
-#[derive(Debug, Display, Error)]
+#[derive(Debug, Error)]
 pub enum ServiceError {
-    DbPool(PoolError),
-    DbDiesel(DieselError),
-}
-
-impl From<DieselError> for ServiceError {
-    fn from(diesel_error: DieselError) -> Self {
-        Self::DbDiesel(diesel_error)
-    }
-}
-
-impl From<PoolError> for ServiceError {
-    fn from(pool_error: PoolError) -> Self {
-        Self::DbPool(pool_error)
-    }
+    #[error("Deadpool Pool error: {0}")]
+    DbPool(#[from] PoolError),
+    #[error("Diesel error : {0}")]
+    DbDiesel(#[from] DieselError),
 }
