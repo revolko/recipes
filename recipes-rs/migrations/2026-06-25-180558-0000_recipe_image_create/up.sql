cc871791cce6f717b4a1be4f200280723f5dd8f7
Juraj Paluba <juraj.palubajp@gmail.com>
2026-06-27 18:19:18 +0200

    fix(migrations): fix foreign key types
    SERIAL is incorrect because it will start the custom counter.

--- recipes-rs/migrations/2026-06-25-180558-0000_recipe_image_create/up.sql
+++ recipes-rs/migrations/2026-06-25-180558-0000_recipe_image_create/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE images (
   id SERIAL PRIMARY KEY,
   bytes BYTEA NOT NULL,
   type VARCHAR NOT NULL,
-  recipe_id SERIAL REFERENCES recipes(id)
+  recipe_id INTEGER NOT NULL UNIQUE REFERENCES recipes(id)
 );
